e74fa58e8e57a973dabbb161f005374bfa3415fe
FarzeenKist <farzeenkistnareddy@gmail.com>
2024-08-31 11:59:39 +0400

    Fix dfx deployment issues and add missing internet identity implementation

--- src/frontend/src/component/Summary.jsx
+++ src/frontend/src/component/Summary.jsx
@@ -1,102 +1,102 @@
-import React,{ useState,useContext,useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { MyContext } from "../Context/myContext";
-import { createOrAdd,updateNote,deleteSingle } from "../utils/endpoint";
-import {AiOutlineEdit,AiOutlineDelete } from "react-icons/ai"
+import { createOrAdd, updateNote, deleteSingle } from "../utils/endpoint";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai"
 import toast from "react-hot-toast";
 
-const Summary=()=>{
-    const [input, setInput] = useState("");
-    const [editor,setEditor]= useState(false);
-    const [id,setId] = useState("")
-    const { summaries,error,loading,getSummary,setSummaries } = useContext(MyContext);
-  
-    useEffect(async()=>{
-      const allSummary = async()=>{
-       await getSummary()
-      } 
-      await allSummary()
-    },[]);
+const Summary = () => {
+  const [input, setInput] = useState("");
+  const [editor, setEditor] = useState(false);
+  const [id, setId] = useState("")
+  const { summaries, error, loading, getSummary, setSummaries } = useContext(MyContext);
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        const note = input;
-        if(note.length >10) {
-          await createOrAdd(note)
-          await getSummary()
-          setInput(" ")
-        }
-      };
-  const editNote=async(note)=>{
-          setInput(note.note);
-          setId(note.id)
-          setEditor(true)
-  }
+  useEffect(async () => {
+    const allSummary = async () => {
+      await getSummary()
+    }
+    await allSummary()
+  }, []);
 
-  const editSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const note = input;
-   if(note && id){
-    const response = await updateNote(input,id);
-    setInput(" ");
-    setId(" ");
-    setEditor(false)
-    if(response.Ok){
-    setSummaries(response.Ok)
+    if (note.length > 10) {
+      await createOrAdd(note)
+      await getSummary()
+      setInput("")
     }
-    if(response.Err){
-      toast.error(`${response.Err.NotFound ||response.Err.Error || response.Err.Unsupported}`);
-    }
-   }
+  };
+  const editNote = async (note) => {
+    setInput(note.note);
+    setId(note.id)
+    setEditor(true)
   }
-  const deleteNote=async(id)=>{
-     const response =await deleteSingle(id)
-     if(response.Err){
+
+  const editSubmit = async (e) => {
+    e.preventDefault();
+    const note = input;
+    if (note && id) {
+      const response = await updateNote(input, id);
+      setInput("");
+      setId("");
+      setEditor(false)
+      if (response.Ok) {
+        setSummaries(response.Ok)
+      }
+      if (response.Err) {
+        toast.error(`${response.Err.NotFound || response.Err.Error || response.Err.Unsupported}`);
+      }
+    }
+  }
+  const deleteNote = async (id) => {
+    const response = await deleteSingle(id)
+    if (response.Err) {
       toast.error(`${response.Err.NotFound || response.Err.Unsupported}`);
     }
-     await getSummary()
+    await getSummary()
   }
- 
-    return(
-        <div className="chat-container">
-        <div className="chat-box">
-          
-          {summaries?.map((msg, index) => (
-            <>
+
+  return (
+    <div className="chat-container">
+      <div className="chat-box">
+
+        {summaries?.map((msg, index) => (
+          <>
 
             <div key={index} className={`chat-message user`}>
               <div>
-              {msg.note}
-              <div className="actionBtn">
-                <button className="editBtn" onClick={()=>{editNote(msg)}}>
-                <AiOutlineEdit/>
-                </button>
-                <button className="deleteBtn" onClick={()=>{deleteNote(msg.id)}}>
-                  <AiOutlineDelete/>
-                </button>
-              </div>
+                {msg.note}
+                <div className="actionBtn">
+                  <button className="editBtn" onClick={() => { editNote(msg) }}>
+                    <AiOutlineEdit />
+                  </button>
+                  <button className="deleteBtn" onClick={() => { deleteNote(msg.id) }}>
+                    <AiOutlineDelete />
+                  </button>
+                </div>
               </div>
             </div>
-            <div  key={index} className={`chat-message ai`}>
-                    <div>{msg.summary}</div>
-                </div>
-            </>
-          ))}
+            <div key={index} className={`chat-message ai`}>
+              <div>{msg.summary}</div>
+            </div>
+          </>
+        ))}
 
-        </div>
-        <form onSubmit={editor?editSubmit:handleSubmit} className="chat-input-box">
-          <textarea
-            type="text"
-            name="note"
-            value={input}
-            onChange={(e) => {setInput(e.target.value)}}
-            placeholder="Type your message..."
-            className="chat-input"
-            row="1"
-          />
-          <button type="submit" className="chat-submit"  disabled={(input.length <10)? true:false}>{loading? "...":"+"}</button>
-        </form>
       </div>
-    )
+      <form onSubmit={editor ? editSubmit : handleSubmit} className="chat-input-box">
+        <textarea
+          type="text"
+          name="note"
+          value={input}
+          onChange={(e) => { setInput(e.target.value) }}
+          placeholder="Type your message..."
+          className="chat-input"
+          row="1"
+        />
+        <button type="submit" className="chat-submit" disabled={(input.length < 10) ? true : false}>{loading ? "..." : "+"}</button>
+      </form>
+    </div>
+  )
 }
 
 export default Summary
--- src/frontend/src/component/TopBar.jsx
+++ src/frontend/src/component/TopBar.jsx
@@ -1,86 +1,84 @@
-import React,{ useContext,useState } from "react";
+import React, { useContext, useState } from "react";
 import { MyContext } from "../Context/myContext";
 import { login, logout } from "../utils/auth";
-import { searchNotes,deleteAll } from "../utils/endpoint";
+import { searchNotes, deleteAll } from "../utils/endpoint";
 import toast from "react-hot-toast";
 
 
-const TopBar=()=>{
-  const [query,setQuerry] = useState(" ");
-  const { setError,setSummaries,loggedIn,getSummary} = useContext(MyContext);
-  
-  const deleteAll2=async()=>{
-    const response=  await deleteAll();
-    if(response.Err){
+const TopBar = () => {
+  const [query, setQuerry] = useState(" ");
+  const { setError, setSummaries, loggedIn, getSummary } = useContext(MyContext);
+
+  const deleteAll2 = async () => {
+    const response = await deleteAll();
+    if (response.Err) {
       toast.error(`${response.Err.NotFound || response.Err.Unsupported}`);
     }
-     
-     if(response.Ok){
+
+    if (response.Ok) {
       window.location.reload()
       toast.success("All deleted successfully");
     }
-     await getSummary()
+    await getSummary()
 
   }
 
-   const submitQuery =async(e)=>{
+  const submitQuery = async (e) => {
     e.preventDefault();
-    const searchWord =query
-   try{
-    if(searchWord.length > 3){
-      const response = await searchNotes(searchWord)
-      if(response.Ok){
-        setSummaries(response.Ok)
-        setError(" ")
-      } 
-      if(response.Err){
-        toast.error(`${response.Err.NotFound}`);
+    const searchWord = query
+    try {
+      if (searchWord.length > 3) {
+        const response = await searchNotes(searchWord)
+        if (response.Ok) {
+          setSummaries(response.Ok)
+          setError(" ")
+        }
+        if (response.Err) {
+          toast.error(`${response.Err.NotFound}`);
+        }
       }
-    }
-  
-   }catch(err){
-    console.log(err)
-     setError(err)
-   }
 
-   }
-    return(
-    
-        <div className="topbar">
-          <div className="topbarContainer">
-             <div className="logo">
-               <img src="src/frontend/src/assets/summaryAI.png" alt="logo image" className="logo-IMG"/>
-             </div>
-          <div className="InputSearch">
-              <div className="SearchContainer">
-                <input 
-                  type="text"
-                  name="query"
-                  value={query}
-                  onChange={(e)=>setQuerry(e.target.value)}
-                  className="InputKey"
-                  placeholder="Search for a note and summary"
-                />
-                <button onClick={submitQuery} 
-                disabled={query.length < 3? true:false}
-                className="BtnSaver">
-                  Search
-                </button>
-             </div></div>
-             <div className="btn-logout">
-                <button className="BtnSave" onClick={deleteAll2}>
-                  Delete All
-                </button>
-                <button className="BtnSave" onClick={login}>
-                  Login 
-                </button>
-                <button className="BtnSave" onClick={logout}>
-                  Logout
-                </button>
-             </div> 
-          </div>
+    } catch (err) {
+      console.log(err)
+      setError(err)
+    }
+
+  }
+  return (
+
+    <div className="topbar">
+      <div className="topbarContainer">
+        <div className="logo">
+          <img src="src/frontend/src/assets/summaryAI.png" alt="logo image" className="logo-IMG" />
         </div>
-    )
+        <div className="InputSearch">
+          <div className="SearchContainer">
+            <input
+              type="text"
+              name="query"
+              value={query}
+              onChange={(e) => setQuerry(e.target.value)}
+              className="InputKey"
+              placeholder="Search for a note and summary"
+            />
+            <button onClick={submitQuery}
+              disabled={query.length < 3 ? true : false}
+              className="BtnSaver">
+              Search
+            </button>
+          </div></div>
+        <div className="btn-logout">
+          <button className="BtnSave" onClick={deleteAll2}>
+            Delete All
+          </button>
+          {loggedIn ? <button className="BtnSave" onClick={logout}>
+            Logout
+          </button> : ""}
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TopBar;
